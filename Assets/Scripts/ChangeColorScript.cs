ce1b5da65c18b055ee9530199c228db0a1b1b2b3
fukuchang <aki-basketplayer21.eye@i.softbank.jp>
2018-11-27 01:58:31 +0900

    add sphere color

---
+++ Assets/Scripts/ChangeColorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorScript : MonoBehaviour {
+
+    Material m_Material;
+    public Color m_color;
+
+	// Use this for initialization
+	void Start () {
+        m_Material = this.GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_Material.color = m_color;
+	}
+}
