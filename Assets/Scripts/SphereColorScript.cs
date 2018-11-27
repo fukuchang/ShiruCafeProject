00311250eb01420ce2c9b34a6721a707f37289cb
fukuchang <aki-basketplayer21.eye@i.softbank.jp>
2018-11-27 20:11:17 +0900

    change Color Sphere

--- Assets/Scripts/SphereColorScript.cs
+++ Assets/Scripts/SphereColorScript.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SphereColorScript : MonoBehaviour {
 
     Material m_Material;
     public Color m_color;
+    Color m_preColor;
 
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         m_Material = this.GetComponent<Renderer>().material;
+        m_preColor = m_color;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        m_Material.color = m_color;
+        if(m_preColor != m_color){
+            m_Material.DOBlendableColor(m_color, 10).SetEase(Ease.OutSine);
+            m_preColor = m_color;
+        }
+       
 	}
 }
